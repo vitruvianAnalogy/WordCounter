7a491d01dd01de8de602554e2ee599c38b396621
Nishant Rawat <vitruvianAnalogy@users.noreply.github.com>
2015-03-22 22:32:01 -0700

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-javac ./src/insight/util/WordCounter.java ./wc_input ./wc_output
+git clone git@github.com:vitruvianAnalogy/WordCounter.git ./home/<Username>/
+cd ./home/<Username>/WordCounter
+
+javac ./home/<username>/WordCounter/src/insight/Folder.java
+echo ${CLASSPATH}
+export CLASSPATH=/home/<username>/Insight/src/insight
+
+javac ./home/<username>/WordCounter/src/insight/util/WordCounter.java
+java ./home/<username>/WordCounter/src/insight/util/WordCounter 
+
+.home/<username>/WordCounter/wc_input .home/<username>/WordCounter/wc_output
