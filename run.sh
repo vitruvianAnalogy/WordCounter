5d641be16f23025d336180cadafba43c9dcc0a00
vitruvianAnalogy <nishantrawat22@gmail.com>
2015-03-22 22:48:42 -0700

    Run.sh changed

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-javac ./src/insight/util/WordCounter.java ./wc_input ./wc_output
+javac ./home/Nishant/WordCounter/src/insight/Folder.java
+echo ${CLASSPATH}
+export CLASSPATH=/home/Nishant/Insight/src/insight
+
+javac ./home/Nishant/WordCounter/src/insight/util/WordCounter.java
+java ./home/Nishant/WordCounter/src/insight/util/WordCounter .home/Nishant/WordCounter/wc_input .home/Nishant/WordCounter/wc_output
